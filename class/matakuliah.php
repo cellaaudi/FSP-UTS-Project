8d2c0600219a9c9be26a3fd2f06dd108f5750ce8
cellaaudi <cellaaudi27@gmail.com>
2022-10-05 02:49:47 +0700

    index drop down

--- class/matakuliah.php
+++ class/matakuliah.php
@@ -1,15 +1,27 @@
 <?php
 require('parent.php');
 
-class Matkul extends Koneksi {
+class Matkul extends Koneksi
+{
     public function __construct($server, $user, $pass, $db)
     {
         parent::__construct($server, $user, $pass, $db);
     }
 
-    public function getMatkul() {
+    public function getMatkul()
+    {
+        $matkul = [];
+
         $sql = "SELECT * FROM matakuliah";
+        $res = $this->con->query($sql);
+
+        while ($row = $res->fetch_assoc()) {
+            $kode = $row['kode'];
+            $nama = $row['nama'];
+
+            $matkul[] = array("kode" => $kode, "nama" => $nama);
+        }
+
+        return $matkul;
     }
 }
-
-?>
--- index.php
+++ index.php
@@ -1,15 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FSP-A UTS Project</title>
+    <link rel="stylesheet" href="css/styles.css">
 </head>
-<body>
-    <?php
-    require('class/');
 
-    ?>
+<body>
+    <form action="" method="post">
+        <div>
+            Mata Kuliah :
+            <select name="selmatkul" id="">
+                <option value=" " style="display: none;">-- Pilih Mata Kuliah --</option>
+                <?php
+                require('class/matakuliah.php');
+                $matakuliah = new Matkul('localhost', 'root', '', 'fsp-uts');
+                $resmatkul = $matakuliah->getMatkul();
+
+                foreach ($resmatkul as $arr) {
+                    echo "<option value='" . $arr['kode'] . "'>" . $arr['nama'] . "</option>";
+                }
+
+                ?>
+            </select>
+            <input type="submit" value="Pilih">
+        </div>
+    </form>
+    <br>
+    <table>
+        <thead>
+            <tr>
+                <td>Peserta</td>
+                <td>E</td>
+                <td>D</td>
+                <td>C</td>
+                <td>BC</td>
+                <td>B</td>
+                <td>AB</td>
+                <td>A</td>
+            </tr>
+        </thead>
+    </table>
 </body>
+
 </html>
